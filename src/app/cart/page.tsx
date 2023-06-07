6ce75ed62c4c2934911989f00f70946128e509ee
Zainulabdeen-101 <bbadshah691@gmail.com>
2023-06-07 20:29:20 +0500

    done

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,26 +1,22 @@
 import { AiOutlineShopping } from "@/components/Icon";
 
 import React from "react";
-export async function cartItems(){
-  try{
-
-    const res = await fetch("/api/cart",{
-      method:"GET"
-    })
-    const cart=await res.json()
-    return cart
-    
-
-  }catch(error){
-    console.log(error)
+export async function cartItems() {
+  try {
+    const res = await fetch("/api/cart", {
+      method: "GET",
+    });
+    const cart = await res.json();
+    return cart;
+  } catch (error) {
+    console.log(error);
   }
-
 }
 
 export default async function page() {
-  const item=await cartItems()
-  console.log(item)
- 
+  const item = await cartItems();
+  console.log(item);
+
   return (
     <>
       {/* {item.length < 1 && (
