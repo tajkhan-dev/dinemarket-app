932fb4d52b22830f398c9d6dd7d72861d614bde0
Zainulabdeen-101 <bbadshah691@gmail.com>
2023-06-07 21:33:42 +0500

    done

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
 const nextConfig = {
     
     images:{
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'cdn.sanity.io',
-                port:''
-            }
-        ]
+       domains:["cdn.sanity.io"]
     }
 }
 
